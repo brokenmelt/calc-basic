bd7a56fe556aee67e1943083d549387a7fa8337f
NAVARAJA KUMARAN G <blackknitebcom@gmail.com>
2026-05-10 01:22:25 -0500

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,13 @@
+a=int(input("enter the num:"))
+b=int(input("enter the num:"))
+c= input("+,-,*,/,% :")
+if c =="+" :
+  print(a+b)
+elif c =="-" :
+  print(a-b)
+elif c =="*" :
+  print(a*b)
+elif c =="/" :
+  print(a/b)
+else:
+  print(a%b)
